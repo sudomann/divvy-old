18910e3764ad90d8c8f522f69f64df122f9254ff
Willy Njundong <njundongw@gmail.com>
2019-04-04 16:13:17 -0400

    restructured api app

--- divvy/urls.py
+++ divvy/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from api import views
+from divvy.api import views
 
 
 router = routers.DefaultRouter()
