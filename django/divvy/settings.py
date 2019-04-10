d57115ece8ae8b2a1e22fab2b5acf488fb75be9a
Willy Njundong <njundongw@gmail.com>
2019-04-09 23:44:31 -0400

    Added djangorestframework_simplejwt

--- django/divvy/settings.py
+++ django/divvy/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
