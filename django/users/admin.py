a811aa421d7a05320d0e5c0b74c10ad476a41fd5
Willy Njundong <njundongw@gmail.com>
2019-04-17 14:14:54 -0400

    Added forms for user management, registered customer user and manager

--- django/users/admin.py
+++ django/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active', 'domain')
+    # consider adding in custom sorting by domain
+    list_filter = ('is_staff', 'is_active', 'domain')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ django/users/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email',
+                  'first_name',
+                  'last_name',
+                  'phone',
+                  'is_minor',
+                  'gender')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',
+                  'first_name',
+                  'last_name',
+                  'phone',
+                  'gender')
