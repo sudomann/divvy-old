2de69e6201b4ca9338fbeff46a009ad30a355c79
Willy Njundong <njundongw@gmail.com>
2019-04-21 03:11:33 -0400

    added empty views for users

--- django/users/views.py
+++ django/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from .models import Domain, Zone
+from .serializers import DomainSerializer, UserSerializer, ZoneSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class DomainList(generics.ListCreateAPIView):
+    queryset = Domain.objects.all()
+    serializer_class = Domain
+
+
+class DomainDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Domain.objects.all()
+    serializer_class = DomainSerializer
