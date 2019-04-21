96fa3a4c077bee60e211b1cd210daa69f652911a
Willy Njundong <njundongw@gmail.com>
2019-04-20 23:40:45 -0400

    model edits
    Migrations for 'trip_calendar':
      trip_calendar/migrations/0005_auto_20190421_0339.py
        - Remove field journey from comment
        - Add field deleted to comment
        - Add field last_modified to comment
        - Add field comments to journey

--- django/trip_calendar/models.py
+++ django/trip_calendar/models.py
@@ -8,11 +8,22 @@ from users.models import Zone
 
 User = get_user_model()
 
+
 class Vehicle(models.Model):
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
-    license_plate = models.CharField(validators=[MinLengthValidator(5)], max_length=8)
+    license_plate = models.CharField(
+        validators=[MinLengthValidator(5)], max_length=8)
     added_time = models.DateTimeField(auto_now_add=True)
     is_roadsafe = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    member = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
 
 class Journey(models.Model):
     details = models.CharField(max_length=100, blank=True)
@@ -24,9 +35,10 @@ class Journey(models.Model):
     actual_departure_time = models.DateTimeField(blank=True)
     actual_arrival_time = models.DateTimeField(blank=True)
     destination = models.ForeignKey(Zone, on_delete=models.PROTECT)
+    comments = models.ManyToManyField(Comment)
 
     class Meta:
-        ordering = ('details',) # TODO: fix this shit
+        ordering = ('details',)  # TODO: fix this shit
 
     def __str__(self):
         return self.details
@@ -36,9 +48,3 @@ class Tracker(models.Model):
     journey = models.OneToOneField(Journey, on_delete=models.CASCADE)
     last_known_location = PointField()
     last_updated = models.DateTimeField(auto_now=True)
-
-class Comment(models.Model):
-    journey = models.OneToOneField(Journey, on_delete=models.CASCADE)
-    content = models.CharField(max_length=100)
-    member = models.ForeignKey(User, on_delete=models.PROTECT)
-    created_time = models.DateTimeField(auto_now_add=True)
