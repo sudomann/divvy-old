6a9e0e323f26969717acd89ecc28633e2e47d2a7
Willy Njundong <njundongw@gmail.com>
2019-04-22 21:58:32 -0400

    new apps created
    pending model redistribution

---
+++ django/domain_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DomainManagementConfig(AppConfig):
+    name = 'domain_management'
---
+++ django/live_tracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LiveTrackerConfig(AppConfig):
+    name = 'live_tracker'
