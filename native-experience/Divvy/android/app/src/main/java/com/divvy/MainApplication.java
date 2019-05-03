2e9e511abc1c25076ade129a385a122c997d2399
Willy Njundong <njundongw@gmail.com>
2019-05-03 17:10:03 -0400

    added support/config for google maps

--- native-experience/Divvy/android/app/src/main/java/com/divvy/MainApplication.java
+++ native-experience/Divvy/android/app/src/main/java/com/divvy/MainApplication.java
@@ -2,6 +2,7 @@ package com.divvy;
 
 import android.app.Application;
 
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AsyncStoragePackage()
+          new AsyncStoragePackage(),
+          new MapsPackage()
       );
     }
 
