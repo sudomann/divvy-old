2f2f9f75d5979ffcbd722a5943b940ed4a129e46
Willy Njundong <njundongw@gmail.com>
2019-05-04 02:33:15 -0400

    set up react-native-navigation

--- native-experience/Divvy/android/app/src/main/java/com/divvy/MainApplication.java
+++ native-experience/Divvy/android/app/src/main/java/com/divvy/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
           new AsyncStoragePackage(),
           new MapsPackage()
       );
